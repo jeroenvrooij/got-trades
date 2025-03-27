57264ce49ef7d4ac21473c23d11131a47a571a82
Jeroen van Rooij <jeroen.vanrooij@gmail.com>
2025-03-27 10:06:08 +0100

    some responsive styling

--- src/Service/ArtVariationsHelper.php
+++ src/Service/ArtVariationsHelper.php
@@ -46,4 +46,47 @@ class ArtVariationsHelper
         
         return '';
     }
+    
+    /**
+     * Same as $this->getHumanReadableArtVariations(), instead it returns a shortened 'code' to be able to
+     * display art variations on smaller viewports. 
+     * 
+     * @param string $artVariations String containing the value of CardPrinting.artVariation. Example: {EA}
+     * 
+     * @return string A shortened human readable description, like 'EA'
+     */
+    public function getShortenedArtVariations(string $artVariations): string
+    {
+        if ($artVariations === '{}') {
+            return '';
+        } elseif ($artVariations === '{EA}') {
+            return 'EA';
+        } elseif ($artVariations === '{AA}') {
+            return 'AA';
+        } elseif ($artVariations === '{FA}') {
+            return 'FA';
+        } elseif ($artVariations === '{AB}') {
+            return 'AB';
+        } elseif ($artVariations === '{AT}') {
+            return 'AT';
+        } elseif ($artVariations === '{AA,EA}') {
+            return 'AA, EA';
+        } elseif ($artVariations === '{AA,FA}') {
+            return 'AA, FA';
+        } elseif ($artVariations === '{AB,EA}') {
+            return 'AB, EA';
+        } elseif ($artVariations === '{AA,AB}') {
+            return 'AA, AB';
+        } elseif ($artVariations === '{AB,AT}') {
+            return 'AB, AT';
+        } elseif ($artVariations === '{AA,AT}') {
+            return 'AA, AT';
+        } elseif ($artVariations === '{HS}') {
+            return 'HS';
+        } elseif ($artVariations === '{AA,HS}') {
+            return 'AA, HS';
+        }
+        
+        return '';
+    }
 }
