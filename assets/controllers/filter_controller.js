0bc9f5e797e8c33a9b179fcecead6410ede23cc8
Jeroen van Rooij <jeroen.vanrooij@gmail.com>
2025-03-30 12:45:38 +0200

    improved the form submitting

--- assets/controllers/filter_controller.js
+++ assets/controllers/filter_controller.js
@@ -1,15 +1,33 @@
 import { Controller } from "@hotwired/stimulus";
 
-  
 export default class extends Controller {
     static targets = ["filterForm"];
 
     connect() {
-        this.element.addEventListener("change", this.submitForm.bind(this));
+        // Debounced form submission function
+        this.debouncedSubmit = this.debounce(this.submitForm.bind(this), 500);
+
+        // Attach event listeners to all form fields
+        this.element.querySelectorAll("input, select, textarea").forEach((field) => {
+            if (field.type === "text") {
+                field.addEventListener("input", this.debouncedSubmit);
+            } else {
+                field.addEventListener("change", this.submitForm.bind(this));
+            }
+        });
     }
 
     submitForm() {
         this.filterFormTarget.requestSubmit();
     }
-}
 
+    debounce(func, delay) {
+        let timeout;
+        return (...args) => {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                func(...args);
+            }, delay);
+        };
+    }
+}
