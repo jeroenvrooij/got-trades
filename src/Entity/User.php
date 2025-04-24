f7427a4e5747c8132a6497eed0b86648e4d558f1
Jeroen van Rooij <jeroen.vanrooij@gmail.com>
2025-04-24 15:39:48 +0200

    lets keep it standard and not tolower the email address

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -71,7 +71,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function setEmail(string $email): static
     {
-        $this->email = strtolower($email);
+        $this->email = $email;
 
         return $this;
     }
